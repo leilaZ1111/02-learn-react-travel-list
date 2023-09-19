f315c40e7abfc3fef57ecb98bedaf689e182708e
leila <leila.zitouni55@gmail.com>
2023-09-19 16:45:55 +0200

    added hook states to form for quantity and description

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
@@ -20,21 +22,41 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return; // if the description is empty, we don't want to add the item
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    // after adding the new item to the list, we want to clear the form so the user can add another item, we do that by resetting the state
+    setDescription('');
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
@@ -70,3 +92,11 @@ function Stats() {
     </footer>
   );
 }
+
+// See how we have used Number() to convert the value to a number instead of a string on <Form<select, the dev tools will show you that the value is a number and it's how we have spoted the potential issie in the first place.
+
+// no need to use Number() for the input value, because it's a text input and the value will always be a string.
+
+// For the states here, we have first define a variable for each state, and then we have used the useState() hook to initialize the state and get the setter function.
+
+// in the element to which we want to bind the state, we have used the "value" attribute to bind the state to the element, and the "onChange" event to update the state when the user changes the value.
