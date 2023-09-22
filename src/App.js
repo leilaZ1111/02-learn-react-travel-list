38d657fd18c8ea5d47804a2f76c1c193e61ae9ef
leila <leila.zitouni55@gmail.com>
2023-09-22 12:42:34 +0200

    added derived state and conditionnal rendering to the stats component

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -113,10 +113,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length; // we can use the length of the array to get the number of items, and we DON'T WANT to store it in a state => it's a derived state!
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>🧳 You have X items on your list, and you already paxed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? 'You got everything! Ready to go ✈️'
+          : `🧳 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
@@ -130,3 +146,6 @@ function Stats() {
 // in the element to which we want to bind the state, we have used the "value" attribute to bind the state to the element, and the "onChange" event to update the state when the user changes the value.
 
 // Checkbox: in order to transform it in a control element, we want to transform the value of the checkbox to a boolean, so we have used the "checked" attribute to bind the state to the element, and the "onChange" event to update the state when the user changes the value.
+
+// In the Footer we have conditionally rendered the message based on the percentage of packed items. We have used a ternary operator to do that.
+// We also use an if statement in the beginning (if (!items.length) => "if they are no items") to check if there are items in the list, and if not, we render a message to invite the user to add some items to the list.
